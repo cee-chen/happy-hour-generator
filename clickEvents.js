688bf2e2d48951278961f9c80602c4884f19c285
Doug Brown <dwbrown89@gmail.com>
2017-05-11 15:58:29 -0700

    from kims code

--- clickEvents.js
+++ clickEvents.js
@@ -3,31 +3,28 @@ const drinks = require('./data/drinks.json');
 const themes = require('./data/drinks.json');
 const playlists = require('./data/drinks.json');
 
+$('#fud').click(function(e) {
+  const url = random(restaurants);
+  window.open(url);
+});
+
+$('#drank').click(function(e) {
+  const url = random(drinks);
+  window.open(url);
+});
+
+$('#musak').click(function(e) {
+  const url = random(playlists);
+  window.open(url);
+});
 
 $('#goButton').click(function(e) {
 
-  const urls = jsonAssemble(
-    random(restaurants),
-    random(drinks),
-    random(playlists)
-  );
+ // TOO, get me a slug
 
- // Slug?
+
+  // '/theme=var'
 });
-
-const urls = jsonAssemble(
-  random(restaurants),
-  random(drinks),
-  random(playlists)
-);
-
-function jsonAssemble(food, drink, playlist) {
-  return {
-    food,
-    drink,
-    playlist
-  };
-}
 
 function random(input) {
   return input[Math.floor(Math.random() * input.length)];
