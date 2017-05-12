97cdfb81133f7cb8a982ed2019346225e44a0959
Austin Kalkins <heartofnova@gmail.com>
2017-05-11 17:39:53 -0700

    more tehmese

--- js/themes.js
+++ js/themes.js
@@ -21,7 +21,7 @@ window.themes = {
     "playlist": "https://open.spotify.com/user/agentstraughan/playlist/2y4udhPNGGhsVf7PNUmm1J",
     "photo": "",
     "restaurant": "",
-    "date": "",
+    "date": ""
   },
   "winter-holiday-party": {
     "name": "Winter Holiday Party",
@@ -29,7 +29,7 @@ window.themes = {
     "playlist": "https://open.spotify.com/user/audettematt7/playlist/5MAL2Uk0SkUvjqWOhN9A8M",
     "photo": "",
     "restaurant": "",
-    "date": "",
+    "date": ""
   },
   "cinco-de-mayo": {
     "name": "Cinco de Mayo",
@@ -37,7 +37,7 @@ window.themes = {
     "playlist": "",
     "photo": "",
     "restaurant": "",
-    "date": "",
+    "date": ""
   },
   "st-pattys-day": {
     "name": "St. Patty's Day",
@@ -45,6 +45,21 @@ window.themes = {
     "playlist": "",
     "photo": "",
     "restaurant": "",
-    "date": "",
+    "date": ""
+  },
+  "laser-gun-fight": {
+    "name": "Laser Gun Fight",
+    "drinks": "http://thedrunkenmoogle.com/post/15353279666/the-portal-two-portal-2-cocktails-ingredients",
+    "playlist": "https://embed.spotify.com/?uri=spotify%3Auser%3Acemsso%3Aplaylist%3A5Lg7WvghYdaP1km1J9NCgw",
+    "photo": "https://i.ytimg.com/vi/1_VMRa8BuwI/maxresdefault.jpg",
+    "restaurant": "https://www.mysweetlilcakes.com/menu",
+    "date": ""
+  },
+  "the-1920s": {
+    "name": "1920s Golden Era",
+    "drinks": "http://www.seriouseats.com/recipes/2007/07/cocktails.html",
+    "playlist": "https://embed.spotify.com/?uri=spotify%3Auser%3A12854791%3Aplaylist%3A7BPY9olDBqAnEYCkETaYBu",
+    "photo": "http://stearnslawpllc.com/wp-content/uploads/2015/04/Sorting-Oysters.jpg",
+    "restaurant": "https://www.trycaviar.com/seattle/coastal-kitchen-5462"
   }
-},
+}
