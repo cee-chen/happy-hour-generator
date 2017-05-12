4367249f5015aaf25ef25b0f58e3c2c2c942a7d2
constancecchen <constance.chen.3@gmail.com>
2017-05-12 03:27:45 -0700

    Build out themes

--- js/themes.js
+++ js/themes.js
@@ -2,64 +2,146 @@ window.themes = {
   "social-media-soiree": {
     "name": "Social Media Soiree",
     "drinks": "http://www.foodandwine.com/recipes/east-jaffa-mule",
-    "playlist": "",
     "photo": "http://eatsmiles.com/wp-content/uploads/2016/11/slide.jpg",
-    "restaurant": "https://www.trycaviar.com/seattle/skillet--capitol-hill-3724",
-    "date": ""
+    "restaurant": "https://www.trycaviar.com/seattle/skillet--capitol-hill-3724"
   },
   "ye-olde-good-time": {
-    "name": "Let's Have A Ye Olde Good Time",
+    "name": "Let’s Have A Ye Olde Good Time",
     "drinks": "http://www.myrecipes.com/recipe/infused-rye-whiskey",
-    "playlist": "https://www.trycaviar.com/seattle/royal-grinders-1437",
     "photo": "https://www.codart.nl/wp-content/uploads/2016/09/Frans_Hals_-_Banquet_of_the_Officers_of_the_St_Hadrian_Civic_Guard_Company_-_WGA11092.jpg",
     "restaurant": "https://www.trycaviar.com/seattle/royal-grinders-1437",
-    "date": ""
+    "playlist": "https://open.spotify.com/embed?uri=spotify:user:grynet_molvin:playlist:4bAquhPmrdqGpfHWLwhloh"
   },
   "cats-of-the-internet": {
     "name": "Cats of the Internet",
-    "drinks": "",
-    "playlist": "https://open.spotify.com/user/agentstraughan/playlist/2y4udhPNGGhsVf7PNUmm1J",
-    "photo": "",
-    "restaurant": "",
-    "date": ""
-  },
-  "winter-holiday-party": {
-    "name": "Winter Holiday Party",
-    "drinks": "",
-    "playlist": "https://open.spotify.com/user/audettematt7/playlist/5MAL2Uk0SkUvjqWOhN9A8M",
-    "photo": "",
-    "restaurant": "",
-    "date": ""
-  },
-  "cinco-de-mayo": {
-    "name": "Cinco de Mayo",
-    "drinks": "",
-    "playlist": "",
-    "photo": "",
-    "restaurant": "",
-    "date": ""
-  },
-  "st-pattys-day": {
-    "name": "St. Patty's Day",
-    "drinks": "",
-    "playlist": "",
-    "photo": "",
-    "restaurant": "",
-    "date": ""
+    "playlist": "https://embed.spotify.com/user/agentstraughan/playlist/2y4udhPNGGhsVf7PNUmm1J",
+    "photo": "https://s-media-cache-ak0.pinimg.com/originals/07/05/ac/0705acb5aa8534854e99cc84701a56f6.jpg",
+    "drinks": "https://www.buzzfeed.com/chelseamarshall/cat-lady-cocktails"
   },
   "laser-gun-fight": {
     "name": "Laser Gun Fight",
     "drinks": "http://thedrunkenmoogle.com/post/15353279666/the-portal-two-portal-2-cocktails-ingredients",
-    "playlist": "https://embed.spotify.com/?uri=spotify%3Auser%3Acemsso%3Aplaylist%3A5Lg7WvghYdaP1km1J9NCgw",
+    "playlist": "https://open.spotify.com/embed?uri=spotify%3Auser%3Acemsso%3Aplaylist%3A5Lg7WvghYdaP1km1J9NCgw",
     "photo": "https://i.ytimg.com/vi/1_VMRa8BuwI/maxresdefault.jpg",
-    "restaurant": "https://www.mysweetlilcakes.com/menu",
-    "date": ""
+    "restaurant": "https://www.mysweetlilcakes.com/menu"
   },
   "the-1920s": {
-    "name": "1920s Golden Era",
+    "name": "1920’s Golden Era",
     "drinks": "http://www.seriouseats.com/recipes/2007/07/cocktails.html",
-    "playlist": "https://embed.spotify.com/?uri=spotify%3Auser%3A12854791%3Aplaylist%3A7BPY9olDBqAnEYCkETaYBu",
+    "playlist": "https://open.spotify.com/embed?uri=spotify%3Auser%3A12854791%3Aplaylist%3A7BPY9olDBqAnEYCkETaYBu",
     "photo": "http://stearnslawpllc.com/wp-content/uploads/2015/04/Sorting-Oysters.jpg",
     "restaurant": "https://www.trycaviar.com/seattle/coastal-kitchen-5462"
-  }
+  },
+  "sesame-street": {
+    "name": "Sesame Street",
+    "photo": "https://cdn-images-1.medium.com/max/1130/0*CpnteKPXMkFbia4E.jpg",
+    "drinks": "https://mixthatdrink.com/big-bird/",
+    "playlist": "https://open.spotify.com/embed?uri=spotify:artist:50bY8HGxT0SuDgJX3AilTT"
+  },
+  "happy-hour-against-humanity": {
+    "name": "Happy Hour Against Humanity",
+    "photo": "https://19818-presscdn-pagely.netdna-ssl.com/wp-content/uploads/b24/8a/05246843c5fe733995d5621f442ec56d.jpg"
+  },
+  "robot-cowboys": {
+    "name": "Robot Cowboys",
+    "photo": "http://i.lv3.hbo.com/assets/images/series/westworld/episodes/media-blast/160819-westworld-s1-blast-07-1280.jpg",
+    "restaurant": "https://postmates.com/sea/twilight-exit-seattle",
+    "drinks": "http://www.myrecipes.com/recipe/infused-rye-whiskey",
+    "playlist": "https://open.spotify.com/embed?uri=spotify:user:g0u1d1e1:playlist:0ixcscG8yW4S7OtzvErdgl"
+  },
+  "fun-pants-friday": {
+    "name": "Fun Pants Friday",
+    "photo": "https://static.pexels.com/photos/165226/pexels-photo-165226.jpeg",
+    "playlist": "https://open.spotify.com/embed?uri=spotify:user:andybaio:playlist:3lqkCp8cpW5opEMhGJOKnM"
+  },
+  "pokemon-party": {
+    "name": "Pokémon Party",
+    "photo": "https://cdn.suwalls.com/wallpapers/games/happy-pikachu-pokemon-50715-1920x1080.jpg",
+    "drinks": "https://www.buzzfeed.com/emmacooke24/gotta-drink-em-all",
+    "playlist": "https://www.youtube.com/playlist?list=PL2D0BE3212F546878"
+  },
+  "only-90s-kids-will-remember": {
+    "name": "Only 90’s Kids Will Remember",
+    "photo": "https://i.imgur.com/CFjU41M.jpg",
+    "restaurant": "https://postmates.com/sea/amante-pizza-seattle",
+    "playlist": "https://open.spotify.com/embed?uri=spotify:user:myplay.com:playlist:3C64V048fGyQfCjmu9TIGA"
+  },
+  "50s-diner-realness": {
+    "name": "50’s Diner Realness",
+    "photo": "https://upload.wikimedia.org/wikipedia/commons/0/0a/A_50's_Style_Diner.jpg",
+    "restaurant": "https://postmates.com/sea/cjs-eatery-seattle",
+    "drinks": "https://www.supercall.com/entertaining/best-drinks-of-the-1950s",
+    "playlist": "https://open.spotify.com/embed?uri=spotify:user:spotify:playlist:7xADHS7Ryc6oMdqBVhNVQ9"
+  },
+  "spaaaaaaace": {
+    "name": "Spaaaaaaace",
+    "photo": "https://wizbangblog.com/wp-content/uploads/2014/05/outer-space.jpg",
+    "drinks": "http://www.space.com/30635-moon-inspired-cocktails-supermoon-lunar-eclipse.html",
+    "playlist": "https://www.youtube.com/watch?v=fWlHjpKmvh8" // FTL soundtrack
+  },
+  "im-on-a-boat": {
+    "name": "I’m On A Boat",
+    "photo": "http://www.nbc.com/sites/nbcunbc/files/files/images/2017/4/26/170420_3505976_Digital_Short__I_m_On_A_Boat.jpg",
+    "restaurant": "https://www.trycaviar.com/seattle/i-love-sushi-catering-3651",
+    "drinks": "http://portcocktails.com/cocktails.html",
+    "playlist": "https://www.youtube.com/watch?v=R7yfISlGLNU"
+  },
+  "taco-or-not-to-taco": {
+    "name": "Taco Or Not To Taco?",
+    "photo": "https://visualhunt.com/photos/xl/1/lunch.jpg",
+    "restaurant": "https://www.trycaviar.com/seattle/rancho-bravo-tacos--capitol-hill-1962",
+    "playlist": "https://www.youtube.com/watch?v=XS9gUUFz6Ps"
+  },
+  "a-taste-of-thai": {
+    "name": "A Taste Of Thai",
+    "photo": "https://visualhunt.com/photos/xl/4/rice-thai-bowl-spoon-clay-white-resort-fragrant.jpg",
+    "restaurant": "https://www.trycaviar.com/seattle/little-thai-2358",
+    "drinks": "http://www.myrecipes.com/recipe/mai-tai-0",
+  },
+  "alaskan-fantasy": {
+    "name": "Alaskan Fantasy",
+    "photo": "https://visualhunt.com/photos/xl/7/mountain-alaska-travelling-rocky-mountains.jpg",
+    "restaurant": "https://www.trycaviar.com/seattle/ivars-salmon-house-restaurant-and-fish-bar-5792",
+    "drinks": "",
+    "playlist": ""
+  },
+  "big-or-little-italy": {
+    "name": "Big Or Little Italy",
+    "photo": "https://images.pexels.com/photos/3498/italian-pizza-restaurant-italy.jpg",
+    "restaurant": "https://postmates.com/sea/the-pasta-bar-seattle",
+    "drinks": "https://www.walksofitaly.com/blog/food-and-wine/italian-cocktails-bellini-spritz",
+    "playlist": "https://open.spotify.com/embed?uri=spotify:user:spotify:playlist:50h6fiaG869obrZlo7a0Ao"
+  },
+  "whats-in-the-can": {
+    "name": "What’s In The Can?",
+    "photo": "https://images.pexels.com/photos/78803/can-food-tuna-78803.jpeg",
+    "restaurant": "https://www.google.com/webhp?hl=en&sa=X&ved=0ahUKEwjpxOTH_enTAhUK-GMKHWZECUIQPAgD#q=canned+food&hl=en&tbs=mr:1,cat:422,ss:44,vw:g&tbm=shop",
+    "drinks": "http://www.thekitchn.com/5-canned-cocktails-for-your-summer-cooler-232461"
+  },
+  "stab-ke-bob": {
+    "name": "Stab-Ke-Bob",
+    "photo": "https://images.pexels.com/photos/111131/meat-vegetables-gemuesepiess-mushrooms-111131.jpeg",
+    "restaurant": "https://postmates.com/sea/petra-mediterranean-bistro-seattle#shish-kabobs",
+  },
+  "poetry-and-wine": {
+    "name": "Poetry & Wine",
+    "drinks": "http://www.reversewinesnob.com/search/label/box-wine/",
+    "playlist": "https://open.spotify.com/user/spotify/playlist/37i9dQZF1DX1DnmMwIpYQk"
+  },
+  "this-is-my-hair": {
+    "name": "This Is My Hair, I Don’t Wear Wigs",
+    "photo": "https://static.pexels.com/photos/4614/woman-morning-bathrobe-bathroom.jpg",
+    "playlist": "https://open.spotify.com/embed?uri=spotify:user:alpertti79:playlist:7DSPBwD0hAPzJioZ5HqCnw"
+  },
+  "tree-huggers-unite": {
+    "name": "Tree Huggers Unite",
+    "photo": "https://cdn.barnimages.com/wp-content/uploads/2016/10/20161006-igor-trepeshchenok-barnimages-3-768x509@2x.jpg",
+    "restaurant": "https://postmates.com/sea/veggie-grill-seattle-2",
+    "playlist": "https://open.spotify.com/embed?uri=spotify:user:sonymusicentertainment:playlist:6JOzvTtlmRl3rEfgMRtkCG"
+  },
+  "noodles-galore": {
+    "name": "Noodle Galore",
+    "photo": "https://visualhunt.com/photos/xl/4/spaghetti-pasta-noodles-eat-food-italy-lunch.jpg",
+    "restaurant": "https://postmates.com/sea/in-the-bowl-vegetarian-noodle-bistro-seattle",
+  },
 }
