51d71a1eded6689a9aba51863cf7e393037ce72f
Austin Kalkins <heartofnova@gmail.com>
2017-05-11 17:35:40 -0700

    add some themes

--- js/themes.js
+++ js/themes.js
@@ -1,4 +1,20 @@
 window.themes = {
+  "social-media-soiree": {
+    "name": "Social Media Soiree",
+    "drinks": "http://www.foodandwine.com/recipes/east-jaffa-mule",
+    "playlist": "",
+    "photo": "https://d2eyrv63e6x6lp.cloudfront.net/wp-content/uploads/2016/04/08141842/iStock_000086308497_Large.jpg",
+    "restaurant": "https://www.trycaviar.com/seattle/skillet--capitol-hill-3724",
+    "date": ""
+  },
+  "ye-olde-good-time": {
+    "name": "Let's Have A Ye Olde Good Time",
+    "drinks": "http://www.myrecipes.com/recipe/infused-rye-whiskey",
+    "playlist": "https://www.trycaviar.com/seattle/royal-grinders-1437",
+    "photo": "https://www.codart.nl/wp-content/uploads/2016/09/Frans_Hals_-_Banquet_of_the_Officers_of_the_St_Hadrian_Civic_Guard_Company_-_WGA11092.jpg",
+    "restaurant": "https://www.trycaviar.com/seattle/royal-grinders-1437",
+    "date": ""
+  },
   "cats-of-the-internet": {
     "name": "Cats of the Internet",
     "drinks": "",
