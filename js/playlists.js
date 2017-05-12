f7a7bab9f5e798634eaad87f990698153d6a2b89
Doug Brown <dwbrown89@gmail.com>
2017-05-12 08:05:48 -0700

    moar

--- js/playlists.js
+++ js/playlists.js
@@ -4,7 +4,7 @@ window.playlists = [
   "https://embed.spotify.com/?uri=spotify%3Auser%3A1215476764%3Aplaylist%3A5L1hFkWAVa7bF7Ryxw40IS", // 1960s oldies
   "https://embed.spotify.com/?uri=spotify%3Auser%3A126025861%3Aplaylist%3A3B0kL0vZ3GNPuFOkGQvz6f", // seattle time playlist
   "https://embed.spotify.com/?uri=spotify%3Auser%3Aspotify%3Aplaylist%3A37i9dQZF1DX58NJL8iVBGW", // swampy folky nonsense
-  "https://embed.spotify.com/?uri=spotify%3Auser%3A1227700256%3Aplaylist%3A5Pgkr5O5sKsxgqPE45BLp2", // someone likes jazz 
+  "https://embed.spotify.com/?uri=spotify%3Auser%3A1227700256%3Aplaylist%3A5Pgkr5O5sKsxgqPE45BLp2", // someone likes jazz
   "https://embed.spotify.com/?uri=spotify%3Auser%3Abluenoterecords%3Aplaylist%3A3WAkqbDCV91pCoPlglLj3l", // Robert Glasper's Blue Note Essentials
   "https://embed.spotify.com/?uri=spotify%3Auser%3Adaiyi%3Aplaylist%3A0k2niR8856pyDg4SHVtv46", // cute hipster things
   "https://embed.spotify.com/?uri=spotify%3Auser%3A12166465404%3Aplaylist%3A6Coeel8nBjY7yjwyya7RBZ", // supa chill
@@ -15,5 +15,13 @@ window.playlists = [
   "https://www.youtube.com/watch?v=wlsdMpnDBn8", // chill
   "https://www.youtube.com/watch?v=zV4G64r91t4&list=RDzV4G64r91t4#t=6", // techno/dance
   "https://www.youtube.com/playlist?list=PLA9A0B7EFC0AEBB3D", // acoustic
-  "https://www.youtube.com/playlist?list=PLC3B0BB09C7C1DC47" // internet memes
+  "https://www.youtube.com/playlist?list=PLC3B0BB09C7C1DC47", // internet memes
+  "https://www.youtube.com/watch?v=eh7lp9umG2I", // HE-MAN HEYEAYEA SONG FOR 10 HOURS
+  "https://www.youtube.com/watch?v=Cv6tuzHUuuk&list=PLYosk6VjN4ias709xn0j4EhcHeO4RQ2yK", // 1987 Top 40
+  "https://open.spotify.com/user/electro_swing/playlist/549lx4U7hl7dSSRMoGIUga", // electro swing 2017
+  "https://open.spotify.com/user/126025861/playlist/3B0kL0vZ3GNPuFOkGQvz6f", // Time Inc, Seattle playlist
+  "https://open.spotify.com/user/12175508356/playlist/7qgNU6BEQlk3otac7sI4jD", // electro swing via Hans
+  "https://open.spotify.com/user/spotify/playlist/37i9dQZF1DXdDe8IDhRIo5", // gypsy swing
+  "https://open.spotify.com/user/spotify/playlist/37i9dQZF1DX6dvuioZhoLo", // "vintage vibes"
+  "https://open.spotify.com/user/spotify/playlist/37i9dQZF1DX504r1DvyvxG", // classic accoustic
 ]
